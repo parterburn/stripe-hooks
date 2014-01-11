59f0e599e49e274595d5b25ca19a81fc5b08fbe5
Paul Arterburn <paul.arterburn@brandfolder.com>
2014-01-11 14:33:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StripeHooks::Application.config.secret_token = 'dd88d213c391f6cf701d6f3f27470ad02114d663ea5ffe6c4301e72af59a7de7d10bb5573eba7533e7924d3b483a1d102a26e4380958361ac0df6c0242e9e07f'
