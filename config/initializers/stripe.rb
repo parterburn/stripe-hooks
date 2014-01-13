d4b501797162147efe366ecf522bb956e87977f4
Paul Arterburn <paul.arterburn@brandfolder.com>
2014-01-12 17:57:10 -0700

    Showing all data

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,16 +3,8 @@ Stripe.api_key = ENV['STRIPE_KEY'] # Set your api key
 StripeEvent.configure do |events|
   events.all do |event|
     subject = "[Stripe] #{event.type} for #{event.data.object.email}"
-    body = "<a href='https://manage.stripe.com/search?query=#{event.data.object.id}'>#{event.data.object.email}</a> has #{event.type}"
+    body = "<a href='https://manage.stripe.com/search?query=#{event.data.object.id}'>#{event.data.object.email}</a> has #{event.type}<br><br>#{debug event.data.object}"
     stripe_mailer(subject, body)
-  end
-end
-
-# Subscriber objects that respond to #call
-
-class CustomerCreated
-  def call(event)
-    # Event handling
   end
 end
 
@@ -31,23 +23,16 @@ end
 
 StripeEvent.configure do |events|
   events.all BillingEventLogger.new(Rails.logger)
-  events.subscribe 'customer.created', CustomerCreated.new
-end
-
-# Subscribing to a namespace of event types
-
-StripeEvent.subscribe 'customer.card.' do |event|
-  # Will be triggered for any customer.card.* events
 end
 
 def stripe_mailer(subject, body)
-  smtpapi =  { :filters => { :clicktrack => { :settings => { :enable => 0 } }, :ganalytics => { :settings => { :enable => 0 } }, :template => { :settings => { :enable => 0 } } } }
+  smtpapi =  { :category => { "Stripe Billing" }, :filters => { :clicktrack => { :settings => { :enable => 0 } }, :ganalytics => { :settings => { :enable => 0 } }, :template => { :settings => { :enable => 0 } } } }
   ActionMailer::Base.mail(
+    :headers['X-SMTPAPI'] => smtpapi.to_json,
     :from => ENV["EMAIL_FROM"],
     :to => ENV["EMAIL_TO"],
     :subject => subject,
     :body => body,
     :content_type => "text/html",
-    :headers['X-SMTPAPI'] => JSON.pretty_generate(smtpapi)
   ).deliver
 end
