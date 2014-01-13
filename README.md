239b46495b4d882a5c95bec66c4c2a51c6e48de5
Paul Arterburn <paul.arterburn@brandfolder.com>
2014-01-12 19:04:39 -0700

    readme

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 stripe-hooks
 ============
 
-A simple hook for Stripe
+A simple email notification app for Stripe events.
+
+============
+
+In order to setup, you'll need to setup environmental variables:
+
+```
+STRIPE_KEY: sk_live_your_stripe_api_key
+SENDGRID_USERNAME: your_sendgrid_username
+SENDGRID_PASSWORD: your_sendgrid_password
+SENDGRID_DOMAIN: domain.com
+EMAIL_FROM: Billing <your@domain.com>
+EMAIL_TO: email@to-send-events-to.com
+```
+
+In the [Webhooks](https://manage.stripe.com/account/webhooks) section of Stripe add the URL of your app: http://your-app.herokuapp.com/process
--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,20 +1,36 @@
 class Notifier < ActionMailer::Base
-  include SendGrid
-
-  sendgrid_category "Stripe Events"
-  sendgrid_disable  :ganalytics, :opentrack, :clicktrack
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_event_email(event)
     @event = event
     headers['x-smtpapi'] = {
       :filters => {
+        
         :template => {
           :settings => {
             :enable => 1,
             :"text/html" => "<% body %>"
           }
+        },
+
+        :clicktrack => {
+          :settings => {
+            :enable => 0
+          }
+        },
+
+        :opentrack => {
+          :settings => {
+            :enable => 0
+          }
+        },
+
+        :subscriptiontrack => {
+          :settings => {
+            :enable => 0
+          }
         }
+
       }
     }.to_json
 
